733962632ca498ea1dcadb6e354f08a350ccc4a8
cyrriuswhite <yannibo@hotmail.fr>
2020-04-15 14:58:59 -0400

    change tab to space

--- nextionassociation.py
+++ nextionassociation.py
@@ -1,152 +1,152 @@
 nextionAssociation_list = [
-	# PAGE 0
-	{
-		"name": "Rum & Coke",
+    # PAGE 0
+    {
+        "name": "Rum & Coke",
         "type":"drink",
-		"page": 0,
-		"component_id": 2
-		
-	}, {
-		"name": "Rum & Sprite",
+        "page": 0,
+        "component_id": 2
+        
+    }, {
+        "name": "Rum & Sprite",
         "type":"drink",
-		"page": 0,
-		"component_id": 3
+        "page": 0,
+        "component_id": 3
     },{
-		"name": "Jack & Coke",
+        "name": "Jack & Coke",
         "type":"drink",
-		"page": 0,
-		"component_id": 4
-		
-	},{
-		"name": "Jack & Sprite",
+        "page": 0,
+        "component_id": 4
+        
+    },{
+        "name": "Jack & Sprite",
         "type":"drink",
-		"page": 0,
-		"component_id": 5
-		
-	},{
-		"name": "Vodka & Sprite",
+        "page": 0,
+        "component_id": 5
+        
+    },{
+        "name": "Vodka & Sprite",
         "type":"drink",
-		"page": 0,
-		"component_id": 6
-		
-	},{
-		"name": "Gin & Sprite",
+        "page": 0,
+        "component_id": 6
+        
+    },{
+        "name": "Gin & Sprite",
         "type":"drink",
-		"page": 0,
-		"component_id": 7
-		
-	},{
-		"name": "Long Island",
+        "page": 0,
+        "component_id": 7
+        
+    },{
+        "name": "Long Island",
         "type":"drink",
-		"page": 0,
-		"component_id": 8
-		
-	},{
-		"name": "TEMPORAIRE",
+        "page": 0,
+        "component_id": 8
+        
+    },{
+        "name": "TEMPORAIRE",
         "type":"drink",
-		"page": 0,
-		"component_id": 9
-	},
-	#Page 1
-	{
-		"name": "rum",
+        "page": 0,
+        "component_id": 9
+    },
+    #Page 1
+    {
+        "name": "rum",
         "type":"shot",
-		"page": 1,
-		"component_id": 2
-	},
-	{
-		"name": "gin",
+        "page": 1,
+        "component_id": 2
+    },
+    {
+        "name": "gin",
         "type":"shot",
-		"page": 1,
-		"component_id": 3
-	},
-	{
-		"name": "vodka",
+        "page": 1,
+        "component_id": 3
+    },
+    {
+        "name": "vodka",
         "type":"shot",
-		"page": 1,
-		"component_id": 4
-	},
-	{
-		"name": "jack",
+        "page": 1,
+        "component_id": 4
+    },
+    {
+        "name": "jack",
         "type":"shot",
-		"page": 1,
-		"component_id": 5
-	},
-	{
-		"name": "russian roulette",
+        "page": 1,
+        "component_id": 5
+    },
+    {
+        "name": "russian roulette",
         "type":"russianRoulette",
-		"page": 1,
-		"component_id": 6
-	},
-	# Page 2
-	{
-		"name": "StatPage",
+        "page": 1,
+        "component_id": 6
+    },
+    # Page 2
+    {
+        "name": "StatPage",
         "type":"statShow",
-		"page": 2,
-		"component_id": 3
-	},
-	{
-		"name": "CleanUp",
+        "page": 2,
+        "component_id": 3
+    },
+    {
+        "name": "CleanUp",
         "type":"cleanUp",
-		"page": 2,
-		"component_id": 6
-	},
-	# Page 4
-	{
-		"name": "NormalSize",
+        "page": 2,
+        "component_id": 6
+    },
+    # Page 4
+    {
+        "name": "NormalSize",
         "type":"sizeOfDrink",
-		"page": 4,
-		"component_id": 3
-	},
-	{
-		"name": "TestSize",
+        "page": 4,
+        "component_id": 3
+    },
+    {
+        "name": "TestSize",
         "type":"sizeOfDrink",
-		"page": 4,
-		"component_id": 4
-	},
-		{
-		"name": "Light",
+        "page": 4,
+        "component_id": 4
+    },
+        {
+        "name": "Light",
         "type":"ratio",
-		"page": 4,
-		"component_id": 5 
-	},
-	{
-		"name": "Strong",
+        "page": 4,
+        "component_id": 5 
+    },
+    {
+        "name": "Strong",
         "type":"ratio",
-		"page": 4,
-		"component_id": 6
-	},
+        "page": 4,
+        "component_id": 6
+    },
 
-	# Page 6
-	{
-		"name": "GlassDetectActivate",
+    # Page 6
+    {
+        "name": "GlassDetectActivate",
         "type":"glassDetect",
-		"page": 6,
-		"component_id": 5
-	},
-	{
-		"name": "GlassDetectDeactivate",
+        "page": 6,
+        "component_id": 5
+    },
+    {
+        "name": "GlassDetectDeactivate",
         "type":"glassDetect",
-		"page": 6,
-		"component_id": 6
-	},
+        "page": 6,
+        "component_id": 6
+    },
 
-	# Page 7
-	{
-		"name": "reload",
+    # Page 7
+    {
+        "name": "reload",
         "type":"statShow",
-		"page": 7,
-		"component_id": 3
-	}
+        "page": 7,
+        "component_id": 3
+    }
 ]
 
 alexaAssociation_list = [
-	{"name": "Rum & Coke", "value": "rum and coke"},
-	{"name": "Rum & Sprite", "value": "rum sprite"},
-	{"name": "Jack & Coke", "value": "jack and coke"},
-	{"name": "Jack & Sprite", "value": "jack sprite"},
-	{"name": "Vodka & Sprite", "value": "vodka sprite"},
-	{"name": "Gin & Sprite", "value": "gin sprite"},
-	{"name": "Long Island", "value": "long island"}
+    {"name": "Rum & Coke", "value": "rum and coke"},
+    {"name": "Rum & Sprite", "value": "rum sprite"},
+    {"name": "Jack & Coke", "value": "jack and coke"},
+    {"name": "Jack & Sprite", "value": "jack sprite"},
+    {"name": "Vodka & Sprite", "value": "vodka sprite"},
+    {"name": "Gin & Sprite", "value": "gin sprite"},
+    {"name": "Long Island", "value": "long island"}
 ]
 
